9b903bdb5e8dda64111620c71587b9667fc9773f
Carl Myers <cmyers@cloudera.com>
2018-01-30 10:08:19 -0800

    RELENG-3066: Pre-commit hook uses GBN based dependencies
    Change-Id: I5bfce65c400f7c22c2b95bdcfdc068627fb9a4b3

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
-# CLOUDERA-BUILD
-mvn clean compile test-compile
+# install mvn settings so dependencies come from a GBN of the latest build
+export CDH_GBN="$(curl "http://builddb.infra.cloudera.com:8080/query?product=cdh&version=6.x&user=jenkins&tag=official")"
+MVN_SETTINGS_FILE="$(mktemp)"
+function cleanup {
+  rm -f "$MVN_SETTINGS_FILE"
+}
+trap cleanup EXIT
+curl -L "http://github.mtv.cloudera.com/raw/CDH/cdh/cdh6.x/gbn-m2-settings.xml" > "$MVN_SETTINGS_FILE"
+
+mvn -U -s "$MVN_SETTINGS_FILE" clean compile test-compile
