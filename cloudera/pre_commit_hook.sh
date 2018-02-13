38b78bf20377d11b0d73e756853e0029aa9ed982
Carl Myers <cmyers@cloudera.com>
2018-02-12 16:35:10 -0800

    RELENG-3084 - modify precommit hook to use mvn-gbn wrapper
    Change-Id: I23cd3cd960714a254b15bbb2eabe97a0fe7efb1b

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -2,13 +2,7 @@
 
 set -ex
 
-# install mvn settings so dependencies come from a GBN of the latest build
-export CDH_GBN="$(curl "http://builddb.infra.cloudera.com:8080/query?product=cdh&version=6.x&user=jenkins&tag=official")"
-MVN_SETTINGS_FILE="$(mktemp)"
-function cleanup {
-  rm -f "$MVN_SETTINGS_FILE"
-}
-trap cleanup EXIT
-curl -L "http://github.mtv.cloudera.com/raw/CDH/cdh/cdh6.x/gbn-m2-settings.xml" > "$MVN_SETTINGS_FILE"
+# activate mvn-gbn wrapper
+mv "$(which mvn-gbn-wrapper)" "$(dirname "$(which mvn-gbn-wrapper)")/mvn"
 
-mvn -U -s "$MVN_SETTINGS_FILE" clean compile test-compile
+mvn -U clean compile test-compile
