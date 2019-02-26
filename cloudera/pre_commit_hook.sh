7abae8b98f7b8a5633fb0aaab7ca64f317c5644f
Zsolt Gyulavari <gyzsolt@cloudera.com>
2019-02-26 16:31:57 +0100

    CDH-78055 removing test failure ignore switch from precommit.sh
    Change-Id: I1fda339214b80fc7782b5228b14edbcd35edc61e

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -5,4 +5,4 @@ set -ex
 # activate mvn-gbn wrapper
 mv "$(which mvn-gbn-wrapper)" "$(dirname "$(which mvn-gbn-wrapper)")/mvn"
 
-mvn --update-snapshots --batch-mode -Dmaven.test.failure.ignore=true -Dtests.nightly=true --fail-at-end clean install
+mvn --update-snapshots --batch-mode -Dtests.nightly=true --fail-at-end clean install
